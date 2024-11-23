2f820540e4012051e8db2437c4d3cf656b005e0a
Jordan C <coker.jordan@gmail.com>
2024-11-23 12:28:52 -0500

    finished

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -6,3 +6,13 @@ currentYearElement.textContent = `© ${currentYear} Jordan Coker - Michigan, USA
 
 const lastModifiedDate = document.lastModified;
 lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+
+// Store the selected elements. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the  button and use a callback function that toggles the list element's list of classes.
+button.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	button.classList.toggle('show');
+});
