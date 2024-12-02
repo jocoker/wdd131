af7f115d7ff3db025f7e426a2fcd2d0e6577f5b1
Jordan C <coker.jordan@gmail.com>
2024-12-01 23:35:44 -0500

    added javascript

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,3 +1,4 @@
+// Display the last modified date for the footer
 const lastModifiedElement = document.getElementById("lastModified");
 const currentYearElement = document.getElementById("year");
 
@@ -6,3 +7,23 @@ currentYearElement.textContent = `© ${currentYear} Jordan Coker - Michigan, USA
 
 const lastModifiedDate = document.lastModified;
 lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+
+// Display windchill factor 
+
+let T = 70;
+let vs = 12;
+let windChill;
+
+if (T <50 && vs > 3) {
+    document.querySelector("#windChill").innerHTML = calculateWincdChill(T, vs);
+} else {
+    document.querySelector("#windChill").innerHTML = "N/A";
+}
+
+
+// Function to calculate windchill
+
+function calculateWincdChill(T, vs) {
+    windChill = 12.12 + (0.6215 * T) - (11.37 * Math.pow(vs, 0.16)) + (0.3965 * T * Math.pow(vs, 0.16));
+    return windChill.toFixed(2)
+}
