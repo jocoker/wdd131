4699624150389fa984c338e3d910844be34df6c2
Jordan C <coker.jordan@gmail.com>
2024-12-09 20:11:29 -0500

    finished

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,9 @@
+// Display the last modified date for the footer
+const lastModifiedElement = document.getElementById("lastModified");
+const currentYearElement = document.getElementById("year");
+
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = `© ${currentYear} Jordan Coker - Michigan, USA`;
+
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
